5bf017bd2f350af2f33452da6b3efbb1cfa686d3
Timon Emken <emken@chalmers.se>
2020-03-29 22:44:48 +0200

    Add a base class for the DM particle.

---
+++ include/DM_Particle.hpp
@@ -0,0 +1,35 @@
+#ifndef __DM_Particle_hpp__
+#define __DM_Particle_hpp__
+
+#include "Target_Nucleus.hpp"
+
+//1. Base class for a DM particle with virtual functions for the cross sections
+class DM_Particle
+{
+	protected:
+		bool low_mass;
+
+		void Print_Summary_Base() const;
+	public:
+		double mass, spin, fractional_density;
+
+		//Constructors:
+		DM_Particle();
+		DM_Particle(double m, double s = 1.0/2.0);
+
+		void Set_Mass(double mDM);
+		void Set_Low_Mass_Mode(bool ldm);
+		void Set_Fractional_Density(double f);
+
+		//Differential cross sections
+		virtual double dSigma_dq2_Nucleus(double q, const Isotope& target, double vDM) const {return 0.0;};
+		virtual double dSigma_dq2_Electron(double q, double vDM) const {return 0.0;};
+		double dSigma_dER_Nucleus(double ER,const Isotope& target,double vDM) const;
+		
+		//Total cross section
+		virtual double Sigma_Nucleus(const Isotope& target, double vDM) const;
+
+		virtual void Print_Summary() const {Print_Summary_Base();};
+};
+
+#endif
