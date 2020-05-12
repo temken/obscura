9c0a9fd84077e572e2ebf95a82e949bc27bda076
Timon Emken <emken@chalmers.se>
2020-05-12 12:19:32 +0200

    Add foundation for unit tests via googletest'

---
+++ tests/test_Astronomy.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include "Astronomy.hpp"
+
+TEST(TestAstronomy, TestFractionalDayssinceJ2000)
+{
+	//ARRANGE
+	int day = 1;
+	int month = 1;
+	int year = 2000;
+	double hour = 12;
+	// ACT & ASSERT
+    ASSERT_DOUBLE_EQ( Fractional_Days_since_J2000(day, month, year, hour),  0.0);
+}
