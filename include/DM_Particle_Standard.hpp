721fe9d32c0967dbd560ab76d95cf9fbada31aba
Timon Emken <emken@chalmers.se>
2020-07-09 12:34:06 +0200

    Update the Set_Mass function of the DM class.
    The cross sections might depend on the DM mass through e.g. effective couplings. That means, changing the mass might also change the cross sections. This is not a desired behaviour of the Set_Mass function.

--- include/DM_Particle_Standard.hpp
+++ include/DM_Particle_Standard.hpp
@@ -35,7 +35,7 @@ class DM_Particle_Standard : public DM_Particle
 	DM_Particle_Standard();
 	DM_Particle_Standard(double mDM, double pre);
 
-	//Primary interaction parameter, in this case the proton or neutron cross section
+	//Primary interaction parameter, in this case the proton, neutron, or electron cross section
 	virtual double Get_Interaction_Parameter(std::string target) const override;
 	virtual void Set_Interaction_Parameter(double par, std::string target) override;
 
