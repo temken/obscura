d825db0f7dda71c19099fd2ae2fe9fef6d58ea4b
Timon Emken <emken@chalmers.se>
2021-01-19 14:37:17 +0100

    Add files for Migdal scattering analyses.

---
+++ tests/test_Direct_Detection_Migdal.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include "Direct_Detection_Migdal.hpp"
+
+using namespace obscura;
+
+TEST(TestDirectDetectionMigdal, TestDefaultConstructor)
+{
+	// ARRANGE
+	DM_Detector_Migdal detector;
+	// ACT & ASSERT
+	ASSERT_EQ(detector.name, "Migdal scattering experiment");
+}
